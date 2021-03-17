2f14f1994a08d63de4d9551eb759af10558ed6c4
João Alves <joao.alves@liberali.local>
2021-03-17 11:58:36 -0400

    Implementação para o usuário

--- AbstractE/Program.cs
+++ AbstractE/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using AbstractE.Entites;
+using System.Collections.Generic;
+using AbstractE.Entites.Enums;
+using System.Globalization;
 
 namespace AbstractE
 {
@@ -6,7 +10,39 @@ namespace AbstractE
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Shape> list = new List<Shape>();
+
+            Console.WriteLine("Enter the number of shapes: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Shape #{i} data: ");
+                Console.WriteLine("Recangle or Circle (r/c)? ");
+                char ch = char.Parse(Console.ReadLine());
+                Console.WriteLine("Color (Black/Blue/Red) :");
+                Color color = Enum.Parse<Color>(Console.ReadLine());
+
+                if(ch == 'r')
+                {
+                    Console.WriteLine("Width: ");
+                    double width = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    Console.WriteLine("Height: ");
+                    double height = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    list.Add(new Rectangle(width, height, color));
+                } else
+                {
+                    Console.WriteLine("Radius: ");
+                    double radius = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    list.Add(new Circle(radius, color));
+                }                
+            }
+            Console.WriteLine();
+            Console.WriteLine("SHAPE AREAS: ");
+            foreach (Shape shape in list)
+            {
+                Console.WriteLine(shape.Area().ToString("F2", CultureInfo.InvariantCulture));
+            }
         }
     }
 }
