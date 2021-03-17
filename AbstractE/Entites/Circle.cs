5ff11159960a67affd9f84cf775175dbd6a0112a
João Alves <joao.alves@liberali.local>
2021-03-17 11:46:59 -0400

    Implementação da classe circle

---
+++ AbstractE/Entites/Circle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AbstractE.Entites.Enums;
+
+namespace AbstractE.Entites
+{
+    class Circle : Shape
+    {
+        public double Radius { get; set; }
+
+        public Circle(double radius, Color color)
+            : base(color)
+        {
+            Radius = radius;
+        }
+
+        public override double Area()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+}
