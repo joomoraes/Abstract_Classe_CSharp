417a421df9fd2eae78e2422e09036a46a0c089a8
João Alves <joao.alves@liberali.local>
2021-03-17 11:34:52 -0400

    Primeiro commit

---
+++ AbstractE/Entites/Enums/color.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractE.Entites.Enums
+{
+    enum Color : int
+    {
+        Blac 1,
+        Blue 2,
+        Red 3
+    }
+}
---
+++ AbstractE/Entites/Shape.cs
@@ -0,0 +1,16 @@
+﻿using AbstractE.Entites.Enums;
+
+namespace AbstractE.Entites
+{
+    abstract class Shape
+    {
+        public Color Color { get; set; }
+
+        public Shape(Color color)
+        {
+            Color = color;
+        }
+
+        public abstract double Area();
+    }
+}
